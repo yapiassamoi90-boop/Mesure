cbfb717893ecccb3545882a5785dd50344785c01
yapiassamoi90-boop <yapiassamoi90@gmail.com>
2026-09-16 17:18:45 +0000

    Add service worker for PWA asset caching
    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'mesure-pwa-v1';
+const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
